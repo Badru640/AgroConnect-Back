bcca6281b6e875bb348e03a58d1fa6ea6e70cd94
InacioMugana <inaciomugaua@gmail.com>
2024-08-14 14:59:07 +0200

    produtctController

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const connectedDB = require("./database/database");
+const cors = require("cors");
+const productRoutes = require("./Routes/productRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(3036, () => {
+  console.log(`Server is running on port localhost:${PORT}`);
+});
---
+++ models/Product.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, required: true },
+  imagem: { type: String, required: true },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
